1d0ccc601061814cbeb38a8cd582230431891370
LearnAny <priyankapal0995@gmail.com>
2022-12-14 15:16:47 +0530

    convert to JSX

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./css/index.css"
 
-function EasyButton({
+export default function EasyButton({
   type = "submit",
   children,
   color,
@@ -71,4 +71,4 @@ function EasyButton({
   );
 }
 
-module.exports.EasyButton = EasyButton
+
